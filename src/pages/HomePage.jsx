f0d1d49828ff36364a068722a4b7ecbc4d465a6b
Julia Mienert <julia.mienert@usu.com>
2026-06-26 22:47:19 +0200

    changed objects

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,19 +7,19 @@ const testimonials = [
   {
     guest: 'Sarah M.',
     imgSrc: './assets/restaurantfood.jpg',
-    rating: '4.5',
+    rating: '4.7',
     text: 'Little Lemon quickly became our favorite neighborhood restaurant. The food is always fresh, and the staff makes every visit feel special.',
   },
   {
     guest: 'David R.',
     imgSrc: './assets/restaurantfood.jpg',
-    rating: '4.5',
+    rating: '5',
     text: 'The flavors are incredible, and the seasonal specials never disappoint. You can truly taste the quality and attention to detail in every dish.',
   },
   {
     guest: 'Emma L.',
     imgSrc: './assets/restaurantfood.jpg',
-    rating: '4.5',
+    rating: '4.8',
     text: 'From the friendly service to the beautifully presented dishes, everything about Little Lemon exceeded our expectations. We can\'t wait to come back!',
   },
   {
@@ -35,19 +35,19 @@ const specials = [
     dish: 'Greek Salat',
     imgSrc: './assets/greek salad.jpg',
     price: '12.99',
-    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+    text: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
   },
   {
-    dish: 'Pasta',
-    imgSrc: './assets/pasta.jpg',
+    dish: 'Bruschetta',
+    imgSrc: './assets/bruschetta.jpg',
     price: '5.99',
-    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+    text: 'Our Bruschetta is made grom grilled bread that has. been smeared with garlic and seasoned with salt and olive oil.',
   },
   {
     dish: 'Lemon Dessert',
     imgSrc: './assets/lemon dessert.jpg',
     price: '5.00',
-    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+    text: 'This comes straight from grandmas\'s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
   },
 ];
 
@@ -57,7 +57,7 @@ const HomePage = () => {
       <Hero
         title="Little Lemon"
         subtitle="Chicago"
-        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
         linkLabel="Reserve a table"
         linkUrl="/booking"
         imgSrc="./assets/restaurantfood.jpg"
@@ -76,7 +76,8 @@ const HomePage = () => {
       <About
         title="Little Lemon"
         subtitle="Chicago"
-        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+        text="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. "
         imgSrc1="./assets/restaurant chef B.jpg"
         imgAlt1="Restaurant"
         imgSrc2="./assets/Mario and Adrian A.jpg"
